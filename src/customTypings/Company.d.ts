abdacdbe51f2f799a64434d84892ab6b7a0fd79a
Callum Watkins <callum@callumwatkins.com>
2022-07-03 14:19:44 +0100

    Add CRNs input section

---
+++ src/customTypings/Company.d.ts
@@ -0,0 +1,9 @@
+export interface Company {
+  crn: string,
+  exists: boolean,
+  name: string,
+  status: string,
+  address: string,
+  confirmationStatementDue: string,
+  accountsDue: string,
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -35,10 +35,47 @@
       </div>
     </div>
   </section>
+  <section class="section" v-if="apiKeyValid">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-half-desktop is-two-thirds-tablet is-full-mobile">
+          <form action="" @submit.prevent="loadCompanies">
+            <fieldset :disabled="loadingCompanies">
+              <div class="field is-narrow">
+                <label for="crns" id="crns-label" class="label">
+                  Company Registration Numbers (CRNs)
+                </label>
+                <div class="control" :class="{'is-loading': loadingCompanies}">
+                  <textarea id="crns" aria-labelledby="#crns-label" rows="10"
+                    class="textarea" :class="{'is-danger': crnsInvalid}"
+                    v-model.lazy="crnsMultiline"></textarea>
+                </div>
+                <p class="help is-danger" v-if="crnsInvalid">
+                  Each line should contain a single CRN
+                </p>
+              </div>
+              <div class="field">
+                <div class="control">
+                  <button type="submit" class="button is-primary"
+                    :class="{'is-loading': loadingCompanies}">
+                    Load
+                  </button>
+                </div>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+      <div v-if="loadingCompanies">
+        <p>Loaded {{ loadedCompaniesCount }} of {{ totalCompaniesCount }} ...</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Company } from '@/customTypings/Company.d';
 // @ is an alias to /src
 
 export default defineComponent({
@@ -51,7 +88,23 @@ export default defineComponent({
       apiKeySaving: false,
       apiKeyValid: false,
       apiKeyInvalid: false,
+      loadingCompanies: false,
+      crnsInvalid: false,
+      crns: [] as string[],
+      loadedCompaniesCount: 0,
+      totalCompaniesCount: 0,
+      loadedCompanies: [] as Company[],
     };
+  },
+  computed: {
+    crnsMultiline: {
+      get(): string {
+        return this.crns.join('\n');
+      },
+      set(newVal: string) {
+        this.crns = newVal.split(/\r?\n/);
+      },
+    },
   },
   methods: {
     async saveApiKey() {
@@ -70,6 +123,34 @@ export default defineComponent({
       headers.append('Authorization', `Basic ${btoa(`${apiKey}:`)}`);
       const response: Response = await fetch(url, { method: 'GET', headers });
       return response;
+    },
+    checkCrn(crn: string): boolean {
+      const regexp = /^(?:[A-Z]{2}[0-9]{6}|[0-9]{8})$/;
+      return regexp.test(crn);
+    },
+    async loadCompanies() {
+      this.finishedLoadingCompanies = false;
+      this.crnsInvalid = false;
+      this.loadedCompanies = [];
+      this.loadedCompaniesCount = 0;
+
+      // Remove trailing empty elements
+      while (this.crns.length > 0 && this.crns[this.crns.length - 1] === '') {
+        this.crns.pop();
+      }
+
+      // Validate CRNs
+      for (let i = 0; i < this.crns.length; i++) {
+        const crn = this.crns[i];
+        if (!this.checkCrn(crn)) {
+          this.crnsInvalid = true;
+          return;
+        }
+      }
+
+      this.totalCompaniesCount = this.crns.length;
+      this.loadingCompanies = true;
+      this.loadingCompanies = false;
     },
   },
 });

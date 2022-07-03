497cda1cedf7abc9d54eac3d7bf70699ddc21070
Callum Watkins <callum@callumwatkins.com>
2022-07-03 14:05:23 +0100

    Add VueTippy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueTippy from 'vue-tippy';
 import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon as fontAwesomeIconComponent } from '@fortawesome/vue-fontawesome';
 
 createApp(App)
   .component('font-awesome-icon', fontAwesomeIconComponent)
+  .use(VueTippy, {
+    defaultProps: {
+      hideOnClick: false,
+    },
+  })
   .use(router)
   .mount('#app');

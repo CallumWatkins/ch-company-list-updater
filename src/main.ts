500e5da0c60bb5adb77c8f9f2b90b337484166b6
Callum Watkins <callum@callumwatkins.com>
2022-07-03 14:04:22 +0100

    Add FontAwesome

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon as fontAwesomeIconComponent } from '@fortawesome/vue-fontawesome';
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+  .component('font-awesome-icon', fontAwesomeIconComponent)
+  .use(router)
+  .mount('#app');

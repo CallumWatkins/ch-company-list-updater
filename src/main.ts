656c48662e40670514d3517ac27ada4048841f4b
Callum Watkins <callum@callumwatkins.com>
2022-07-03 14:11:58 +0100

    Add API key input section

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+/* eslint-disable import/order */
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueTippy from 'vue-tippy';
 import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon as fontAwesomeIconComponent } from '@fortawesome/vue-fontawesome';
+import { faKey, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+fontAwesomeLibrary.add(faKey, faCheck);
 
 createApp(App)
   .component('font-awesome-icon', fontAwesomeIconComponent)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-half-desktop is-two-thirds-tablet is-full-mobile">
+          <form action="" @submit.prevent="saveApiKey">
+            <fieldset :disabled="apiKeySaving || apiKeyValid">
+              <div class="field is-narrow">
+                <label for="api-key" id="api-key-label" class="label">API Key</label>
+                <div class="control has-icons-left" :class="{'has-icons-right': apiKeyValid}">
+                  <input type="password" id="api-key" aria-labelledby="#api-key-label"
+                    class="input" :class="{'is-danger': apiKeyInvalid}" v-model="apiKey">
+                  <span class="icon is-small is-left">
+                    <font-awesome-icon icon="fa-solid fa-key" />
+                  </span>
+                  <span class="icon is-small is-right" v-if="apiKeyValid">
+                    <font-awesome-icon icon="fa-solid fa-check" />
+                  </span>
+                </div>
+                <p class="help is-danger" v-if="apiKeyInvalid">
+                  Invalid API key
+                </p>
+              </div>
+              <div class="field" v-if="!apiKeyValid">
+                <div class="control">
+                  <button type="submit" class="button is-primary"
+                    :class="{'is-loading': apiKeySaving}">
+                    Save
+                  </button>
+                </div>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+  },
+  data() {
+    return {
+      apiKey: '',
+      apiKeySaving: false,
+      apiKeyValid: false,
+      apiKeyInvalid: false,
+    };
+  },
+  methods: {
+    async saveApiKey() {
+      this.apiKeySaving = true;
+      const valid = await this.testApiKey();
+      this.apiKeyValid = valid;
+      this.apiKeyInvalid = !valid;
+      this.apiKeySaving = false;
+    },
+    async testApiKey(): Promise<boolean> {
+      const response: Response = await this.apiRequest('https://api.company-information.service.gov.uk/search?q=a&items_per_page=1', this.apiKey);
+      return response.ok;
+    },
+    async apiRequest(url: string, apiKey: string): Promise<Response> {
+      const headers = new Headers();
+      headers.append('Authorization', `Basic ${btoa(`${apiKey}:`)}`);
+      const response: Response = await fetch(url, { method: 'GET', headers });
+      return response;
+    },
   },
 });
 </script>

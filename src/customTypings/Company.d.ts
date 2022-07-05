9fbdd39ffed5f51fc2c0fdbdea61ce0491acc9ea
Callum Watkins <callum@callumwatkins.com>
2022-07-05 23:09:49 +0100

    Delete old Company interface

--- src/customTypings/Company.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Company {
-  crn: string,
-  exists: boolean,
-  name: string,
-  status: string,
-  address: string,
-  confirmationStatementDue: string,
-  accountsDue: string,
-}

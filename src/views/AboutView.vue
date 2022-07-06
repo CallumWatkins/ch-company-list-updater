9c554bb27b16ad22f1fd2ad48d9c557d3f841073
Callum Watkins <callum@callumwatkins.com>
2022-07-06 04:52:46 +0100

    Add about text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title is-3">About</h1>
+      <p>
+        This application uses the <a href="https://developer-specs.company-information.service.gov.uk/companies-house-public-data-api/reference">
+          Companies House Public Data API</a> to load the latest company information for a list of companies.
+      </p>
+      <p>To obtain an API key, visit <a href="https://developer.company-information.service.gov.uk/">
+        https://developer.company-information.service.gov.uk</a>.</p>
+    </div>
+  </section>
 </template>

cb89e489f8e3dc531d73d077dc5c4e595700478f
Callum Watkins <callum@callumwatkins.com>
2022-07-06 03:47:43 +0100

    Add navbar hamburger menu for mobile users

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,15 @@
         <router-link to="/" class="navbar-item">
           <img src="./assets/logo.png" alt="" height="28">
         </router-link>
+        <a role="button" class="navbar-burger" :class="{ 'is-active': navbarHamburgerOpen }"
+          @click="navbarHamburgerOpen = !navbarHamburgerOpen" @keypress.enter="navbarHamburgerOpen = !navbarHamburgerOpen"
+          aria-label="menu" :aria-expanded="navbarHamburgerOpen" tabindex="0">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div class="navbar-menu">
+      <div class="navbar-menu" :class="{ 'is-active': navbarHamburgerOpen }">
         <div class="navbar-start">
           <router-link to="/" class="navbar-item">Home</router-link>
           <router-link to="/about" class="navbar-item">About</router-link>
@@ -23,6 +30,19 @@
     </div>
   </footer>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      navbarHamburgerOpen: false,
+    };
+  },
+});
+</script>
 
 <style lang="scss">
 @import "@/assets/styles/styles.scss";

b8c229c2484b907db4b61f4ab5aa1ffd549b5b50
Callum Watkins <callum@callumwatkins.com>
2022-07-07 20:31:05 +0100

    Revert "Reduce dist size with property name minification"
    This reverts commit ee554a2cb69607a16722af274c6250f3fbe54fce.

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  chainWebpack(config) {
-    config.optimization.minimizer('terser').tap((args) => {
-      const terserOptions = args[0].terserOptions;
-      terserOptions.mangle = {
-        ...terserOptions.mangle,
-        properties: true,
-      };
-      return args;
-    });
-  },
   devServer: {
     allowedHosts: 'all',
   },

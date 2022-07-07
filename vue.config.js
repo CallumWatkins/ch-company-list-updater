ee554a2cb69607a16722af274c6250f3fbe54fce
Callum Watkins <callum@callumwatkins.com>
2022-07-07 20:21:35 +0100

    Reduce dist size with property name minification

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,16 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
+  chainWebpack(config) {
+    config.optimization.minimizer('terser').tap((args) => {
+      const terserOptions = args[0].terserOptions;
+      terserOptions.mangle = {
+        ...terserOptions.mangle,
+        properties: true,
+      };
+      return args;
+    });
+  },
   devServer: {
     allowedHosts: 'all',
   },
